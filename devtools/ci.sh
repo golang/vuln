e7eb6ac4430feba895bf0d35242ce97c8c47eeef
Jonathan Amsterdam <jba@google.com>
2021-12-01 09:13:07 -0500

    devtools/ci.sh: script for kokoro ci
    Set up a script that kokoro will run.
    
    Right now it just calls all.bash, but it will be nice to have this
    extra level of indirection so we don't have to modify the kokoro
    config in the Google-internal repo.
    
    Change-Id: I87aa099d4d41e6fccc019b31cb24fdf48d6ff815
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/368114
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Julie Qiu <julie@golang.org>

---
+++ devtools/ci.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright 2021 The Go Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# This script will be run by our Kokoro job, whose configuration
+# can be found under go/kokoro in Google3's internal repo.
+# It is run from the repo's root directory.
+
+./all.bash
