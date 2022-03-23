itsManjeet/exp

vulncheck: replace packages.Package with vulncheck.Package
Change-Id: I2f1b50bbc101fdf6aaf49271d51f41cdf4be5cc1

vulncheck/source_test.go
@@ -109,7 +109,7 @@ func TestImportsOnly(t *testing.T) {
 		Client:      testClient,
 		ImportsOnly: true,
 	}
-	result, err := Source(pkgs, cfg)
+	result, err := Source(Packages(pkgs), cfg)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -345,7 +345,7 @@ func TestCallGraph(t *testing.T) {
 	cfg := &Config{
 		Client: testClient,
 	}
-	result, err := Source(pkgs, cfg)
+	result, err := Source(Packages(pkgs), cfg)
 	if err != nil {
 		t.Fatal(err)
 	}

golang/vuln

packages.Package can consume a lot of memory for some clients,
so we use its trimmed version vulncheck.Packages instead.
Cherry-picked: https://go-review.googlesource.com/c/exp/+/369135
Change-Id: I530174219824879ccc39ca0f3abb2c9d6c71a3d0
Reviewed-on: https://go-review.googlesource.com/c/vuln/+/395045
Trust: Julie Qiu <julie@golang.org>
Run-TryBot: Julie Qiu <julie@golang.org>
Reviewed-by: Jonathan Amsterdam <jba@google.com>

vulncheck/source_test.go
@@ -109,7 +109,7 @@ func TestImportsOnly(t *testing.T) {
 		Client:      testClient,
 		ImportsOnly: true,
 	}
-	result, err := Source(pkgs, cfg)
+	result, err := Source(Convert(pkgs), cfg)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -345,7 +345,7 @@ func TestCallGraph(t *testing.T) {
 	cfg := &Config{
 		Client: testClient,
 	}
-	result, err := Source(pkgs, cfg)
+	result, err := Source(Convert(pkgs), cfg)
 	if err != nil {
 		t.Fatal(err)
 	}