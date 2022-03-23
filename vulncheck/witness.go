itsManjeet/exp

vulncheck: add witness search logic for call stacks
Change-Id: Ib45c5c6aad347e1bcecb39ca4a62f589b4766677

vulncheck/witness.go
@@ -2,7 +2,13 @@ package vulncheck
 
 import (
 	"container/list"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
 	"sync"
+
+	"golang.org/x/tools/go/packages"
 )
 
 // ImportChain is sequence of import paths starting with
@@ -121,4 +127,193 @@ type StackEntry struct {
 	// Call provides information on the call site inducing this stack frame.
 	// nil when the frame represents an entry point of the stack.
 	Call *CallSite
+}
+
+// ImportChains performs a BFS search of res.RequireGraph for imports of vulnerable
+// packages. Search is performed for each vulnerable package in res.Vulns. The search
+// starts at a vulnerable package and goes up until reaching an entry package in
+// res.ImportGraph.Entries, hence producing an import chain. During the search, a
+// package is visited only once to avoid analyzing every possible import chain.
+// Hence, not all possible vulnerable import chains are reported.
+//
+// Note that the resulting map produces an import chain for each Vuln. Thus, a Vuln
+// with the same PkgPath will have the same list of identified import chains.
+//
+// The reported import chains are ordered by how seemingly easy is to understand
+// them. Shorter import chains appear earlier in the returned slices.
+
+// CallStacks performs a BFS search of res.CallGraph for calls to vulnerable
+// symbols. Search is performed for each vulnerable symbol in res.Vulns. The
+// search starts at the vulnerable symbol and goes up until reaching an entry
+// function or method in res.CallGraph.Entries, hence producing a call stack.
+// During the search for a symbol, each function is visited at most once to avoid
+// potential exponential explosion. Hence, not all possible call stacks are reported.
+//
+// The reported call stacks are ordered by how seemingly easy is to understand
+// them. In general, shorter call stacks with less dynamic call sites appear
+// earlier in the returned call stack slices.
+func CallStacks(res *Result) map[*Vuln][]CallStack {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	stacksPerVuln := make(map[*Vuln][]CallStack)
+	for _, vuln := range res.Vulns {
+		v := vuln
+		wg.Add(1)
+		go func() {
+			cs := callStacks(v.CallSink, res)
+			// sort call stacks by the estimated value to the user
+			sort.SliceStable(cs, func(i int, j int) bool { return stackCompare(cs[i], cs[j]) })
+			mu.Lock()
+			stacksPerVuln[v] = cs
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	return stacksPerVuln
+}
+
+// callStacks finds representative call stacks
+// for vulnerable symbol identified with vulnSinkID.
+func callStacks(vulnSinkID int, res *Result) []CallStack {
+	if vulnSinkID == 0 {
+		return nil
+	}
+
+	entries := make(map[int]bool)
+	for _, e := range res.Calls.Entries {
+		entries[e] = true
+	}
+
+	var stacks []CallStack
+	seen := make(map[int]bool)
+
+	queue := list.New()
+	queue.PushBack(&callChain{f: res.Calls.Functions[vulnSinkID]})
+
+	for queue.Len() > 0 {
+		front := queue.Front()
+		c := front.Value.(*callChain)
+		queue.Remove(front)
+
+		f := c.f
+		if seen[f.ID] {
+			continue
+		}
+		seen[f.ID] = true
+
+		for _, cs := range f.CallSites {
+			callee := res.Calls.Functions[cs.Parent]
+			newS := &callChain{f: callee, call: cs, child: c}
+			if entries[callee.ID] {
+				stacks = append(stacks, newS.CallStack())
+			}
+
+			queue.PushBack(newS)
+		}
+	}
+	return stacks
+}
+
+// callChain models an chain of function calls.
+type callChain struct {
+	call  *CallSite
+	f     *FuncNode
+	child *callChain
+}
+
+// CallStack converts callChain to CallStack type.
+func (c *callChain) CallStack() CallStack {
+	if c == nil {
+		return nil
+	}
+	return append(CallStack{StackEntry{Function: c.f, Call: c.call}}, c.child.CallStack()...)
+}
+
+// weight computes an approximate measure of how easy is to understand the call
+// stack when presented to the client as a witness. The smaller the value, the more
+// understendeable the stack is. Currently defined as the number of unresolved
+// call sites in the stack.
+func weight(stack CallStack) int {
+	w := 0
+	for _, e := range stack {
+		if e.Call != nil && !e.Call.Resolved {
+			w += 1
+		}
+	}
+	return w
+}
+
+// for assesing confidence level of call stacks.
+var stdPackages = make(map[string]bool)
+var loadStdsOnce sync.Once
+
+func isStdPackage(pkg string) bool {
+	loadStdsOnce.Do(func() {
+		pkgs, err := packages.Load(nil, "std")
+		if err != nil {
+			log.Printf("warning: unable to fetch list of std packages, call stack accuracy might be affected: %v", err)
+		}
+
+		for _, p := range pkgs {
+			stdPackages[p.PkgPath] = true
+		}
+	})
+	return stdPackages[pkg]
+}
+
+// confidence computes an approximate measure of whether the stack
+// is realizeable in practice. Currently, it equals the number of call
+// sites in stack that go through standard libraries. Such call stacks
+// have been experimentally shown to often result in false positives.
+func confidence(stack CallStack) int {
+	c := 0
+	for _, e := range stack {
+		if isStdPackage(e.Function.PkgPath) {
+			c += 1
+		}
+	}
+	return c
+}
+
+// stackCompare compares two call stacks in terms of their estimated
+// value to the user. Shorter stacks generally come earlier in the ordering.
+//
+// Two stacks are lexicographically ordered by:
+// 1) their estimated level of confidence in being a real call stack,
+// 2) their length, and 3) the number of dynamic call sites in the stack.
+func stackCompare(s1, s2 CallStack) bool {
+	if confidence(s1) != confidence(s2) {
+		return confidence(s1) < confidence(s2)
+	}
+
+	if len(s1) != len(s2) {
+		return len(s1) < len(s2)
+	}
+
+	if weight(s1) != weight(s2) {
+		return weight(s1) < weight(s2)
+	}
+	// At this point we just need to make sure the ordering is deterministic.
+	// TODO(zpavlinovic): is there a more meaningful additional ordering?
+	return stackStrCompare(s1, s2)
+}
+
+// stackStrCompare compares string representation of stacks.
+func stackStrCompare(s1, s2 CallStack) bool {
+	// Creates a unique string representation of a call stack
+	// for comparison purposes only.
+	stackStr := func(s CallStack) string {
+		var sStr []string
+		for _, cs := range s {
+			if cs.Call == nil {
+				sStr = append(sStr, cs.Function.String())
+			} else {
+				sStr = append(sStr, fmt.Sprintf("%s[%s]", cs.Function.String(), cs.Call.Pos))
+			}
+		}
+		return strings.Join(sStr, "->")
+	}
+	return strings.Compare(stackStr(s1), stackStr(s2)) <= 0
 }

golang/vuln

The CL adds API for traversal of call graph in search of call stacks
that can serve as witnesses for uses of vulnerable symbols.
Cherry-picked: https://go-review.googlesource.com/c/exp/+/381777
Change-Id: Ib290716122b96f25e0f74a85f942dbde242ab04d
Reviewed-on: https://go-review.googlesource.com/c/vuln/+/395058
Trust: Julie Qiu <julie@golang.org>
Run-TryBot: Julie Qiu <julie@golang.org>
Reviewed-by: Jonathan Amsterdam <jba@google.com>

vulncheck/witness.go
@@ -6,6 +6,9 @@ package vulncheck
 
 import (
 	"container/list"
+	"fmt"
+	"sort"
+	"strings"
 	"sync"
 )
 
@@ -14,18 +17,18 @@ import (
 // known vulnerabilities.
 type ImportChain []*PkgNode
 
-// ImportChains performs a BFS search of res.RequireGraph for imports of vulnerable
-// packages. Search is performed for each vulnerable package in res.Vulns. The search
-// starts at a vulnerable package and goes up until reaching an entry package in
-// res.ImportGraph.Entries, hence producing an import chain. During the search, a
-// package is visited only once to avoid analyzing every possible import chain.
-// Hence, not all possible vulnerable import chains are reported.
-//
-// Note that the resulting map produces an import chain for each Vuln. Thus, a Vuln
-// with the same PkgPath will have the same list of identified import chains.
-//
-// The reported import chains are ordered by how seemingly easy is to understand
+// ImportChains lists import chains for each vulnerability in res. The
+// reported chains are ordered by how seemingly easy is to understand
 // them. Shorter import chains appear earlier in the returned slices.
+//
+// ImportChains does not list all import chains for a vulnerability.
+// It performs a BFS search of res.RequireGraph starting at a vulnerable
+// package and going up until reaching an entry package in res.ImportGraph.Entries.
+// During this search, a package is visited only once to avoid analyzing
+// every possible import chain.
+//
+// Note that the resulting map produces an import chain for each Vuln. Vulns
+// with the same PkgPath will have the same list of identified import chains.
 func ImportChains(res *Result) map[*Vuln][]ImportChain {
 	// Group vulns per package.
 	vPerPkg := make(map[int][]*Vuln)
@@ -125,4 +128,176 @@ type StackEntry struct {
 	// Call provides information on the call site inducing this stack frame.
 	// nil when the frame represents an entry point of the stack.
 	Call *CallSite
+}
+
+// CallStacks lists call stacks for each vulnerability in res. The listed call
+// stacks are ordered by how seemingly easy is to understand them. In general,
+// shorter call stacks with less dynamic call sites appear earlier in the returned
+// call stack slices.
+//
+// CallStacks does not report every possible call stack for a vulnerable symbol.
+// It performs a BFS search of res.CallGraph starting at the symbol and going up
+// until reaching an entry function or method in res.CallGraph.Entries. During
+// this search, each function is visited at most once to avoid potential
+// exponential explosion, thus skipping some call stacks.
+func CallStacks(res *Result) map[*Vuln][]CallStack {
+	var (
+		wg sync.WaitGroup
+		mu sync.Mutex
+	)
+	stacksPerVuln := make(map[*Vuln][]CallStack)
+	for _, vuln := range res.Vulns {
+		vuln := vuln
+		wg.Add(1)
+		go func() {
+			cs := callStacks(vuln.CallSink, res)
+			// sort call stacks by the estimated value to the user
+			sort.SliceStable(cs, func(i int, j int) bool { return stackLess(cs[i], cs[j]) })
+			mu.Lock()
+			stacksPerVuln[vuln] = cs
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	return stacksPerVuln
+}
+
+// callStacks finds representative call stacks
+// for vulnerable symbol identified with vulnSinkID.
+func callStacks(vulnSinkID int, res *Result) []CallStack {
+	if vulnSinkID == 0 {
+		return nil
+	}
+
+	entries := make(map[int]bool)
+	for _, e := range res.Calls.Entries {
+		entries[e] = true
+	}
+
+	var stacks []CallStack
+	seen := make(map[int]bool)
+
+	queue := list.New()
+	queue.PushBack(&callChain{f: res.Calls.Functions[vulnSinkID]})
+
+	for queue.Len() > 0 {
+		front := queue.Front()
+		c := front.Value.(*callChain)
+		queue.Remove(front)
+
+		f := c.f
+		if seen[f.ID] {
+			continue
+		}
+		seen[f.ID] = true
+
+		for _, cs := range f.CallSites {
+			callee := res.Calls.Functions[cs.Parent]
+			nStack := &callChain{f: callee, call: cs, child: c}
+			if entries[callee.ID] {
+				stacks = append(stacks, nStack.CallStack())
+			}
+			queue.PushBack(nStack)
+		}
+	}
+	return stacks
+}
+
+// callChain models a chain of function calls.
+type callChain struct {
+	call  *CallSite // nil for entry points
+	f     *FuncNode
+	child *callChain
+}
+
+// CallStack converts callChain to CallStack type.
+func (c *callChain) CallStack() CallStack {
+	if c == nil {
+		return nil
+	}
+	return append(CallStack{StackEntry{Function: c.f, Call: c.call}}, c.child.CallStack()...)
+}
+
+// weight computes an approximate measure of how easy is to understand the call
+// stack when presented to the client as a witness. The smaller the value, the more
+// understandable the stack is. Currently defined as the number of unresolved
+// call sites in the stack.
+func weight(stack CallStack) int {
+	w := 0
+	for _, e := range stack {
+		if e.Call != nil && !e.Call.Resolved {
+			w += 1
+		}
+	}
+	return w
+}
+
+func isStdPackage(pkg string) bool {
+	if pkg == "" {
+		return false
+	}
+	// std packages do not have a "." in their path. For instance, see
+	// Contains in pkgsite/+/refs/heads/master/internal/stdlbib/stdlib.go.
+	if i := strings.IndexByte(pkg, '/'); i != -1 {
+		pkg = pkg[:i]
+	}
+	return !strings.Contains(pkg, ".")
+}
+
+// confidence computes an approximate measure of whether the stack
+// is realizeable in practice. Currently, it equals the number of call
+// sites in stack that go through standard libraries. Such call stacks
+// have been experimentally shown to often result in false positives.
+func confidence(stack CallStack) int {
+	c := 0
+	for _, e := range stack {
+		if isStdPackage(e.Function.PkgPath) {
+			c += 1
+		}
+	}
+	return c
+}
+
+// stackLess compares two call stacks in terms of their estimated
+// value to the user. Shorter stacks generally come earlier in the ordering.
+//
+// Two stacks are lexicographically ordered by:
+// 1) their estimated level of confidence in being a real call stack,
+// 2) their length, and 3) the number of dynamic call sites in the stack.
+func stackLess(s1, s2 CallStack) bool {
+	if c1, c2 := confidence(s1), confidence(s2); c1 != c2 {
+		return c1 < c2
+	}
+
+	if len(s1) != len(s2) {
+		return len(s1) < len(s2)
+	}
+
+	if w1, w2 := weight(s1), weight(s2); w1 != w2 {
+		return w1 < w2
+	}
+	// At this point we just need to make sure the ordering is deterministic.
+	// TODO(zpavlinovic): is there a more meaningful additional ordering?
+	return stackStrLess(s1, s2)
+}
+
+// stackStrLess compares string representation of stacks.
+func stackStrLess(s1, s2 CallStack) bool {
+	// Creates a unique string representation of a call stack
+	// for comparison purposes only.
+	stackStr := func(stack CallStack) string {
+		var stackStr []string
+		for _, cs := range stack {
+			s := cs.Function.String()
+			if cs.Call != nil && cs.Call.Pos != nil {
+				p := cs.Call.Pos
+				s = fmt.Sprintf("%s[%s:%d:%d:%d]", s, p.Filename, p.Line, p.Column, p.Offset)
+			}
+			stackStr = append(stackStr, s)
+		}
+		return strings.Join(stackStr, "->")
+	}
+	return strings.Compare(stackStr(s1), stackStr(s2)) <= 0
 }