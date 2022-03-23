4fe4bee419efb3a1a2f83aca6017fb57effa7458
Julie Qiu <julie@golang.org>
2022-03-23 14:47:32 -0400

    vulncheck: don't define Binary in go1.18
    Binary uses the debug/buildinfo package, which isn't introduced until
    go1.18.
    
    Change-Id: Ic724c12e6d8da649c325f9ed9ee87b2044a6a305
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/395095
    Trust: Julie Qiu <julie@golang.org>
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- vulncheck/internal/binscan/exe.go
+++ vulncheck/internal/binscan/exe.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build go1.18
+// +build go1.18
+
 package binscan
 
 // This file is a somewhat modified version of cmd/go/internal/version/exe.go
--- vulncheck/internal/binscan/scan.go
+++ vulncheck/internal/binscan/scan.go
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build go1.18
+// +build go1.18
+
 // Package binscan contains methods for parsing Go binary files for the purpose
 // of extracting module dependency and symbol table information.
 package binscan
