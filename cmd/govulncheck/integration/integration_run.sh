db381a628bb672643cb6ad91d2e360481e1fffaf
Zvonimir Pavlinovic <zpavlinovic@google.com>
2022-04-12 10:09:43 -0700

    cmd/govulncheck: runs govulncheck on k8s in integration image
    integration_run.sh now clones kubernetes and executes govulncheck on it.
    
    Change-Id: I80eeb07ec597f6c007916b61f93c03b22993629b
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/400415
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Zvonimir Pavlinovic <zpavlinovic@google.com>
    Auto-Submit: Zvonimir Pavlinovic <zpavlinovic@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- cmd/govulncheck/integration/integration_run.sh
+++ cmd/govulncheck/integration/integration_run.sh
@@ -3,5 +3,29 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-# TODO(zpavlinovic): add integration checks
-govulncheck -h
+# Clone kubernetes to a dedicated directory.
+dir="$GOPATH/src/kubernetes"
+if [ -d $dir ]; then
+  echo "Destination kubernetes already exists. Using the existing code."
+else
+  git clone https://github.com/kubernetes/kubernetes.git "${dir}"
+fi
+
+# Checkout kubernetes version v1.15.11 that
+# is known to have vulnerabilities.
+pushd $dir
+cd pkg
+git checkout tags/v1.15.11
+govulncheck ./...
+exitcode=$?
+popd
+
+# There should be some findings, which govulncheck
+# indicates by exit code 3.
+#
+# TODO(zpavlinovic): check if findings are as expected.
+if [ ${exitcode} -ne 3 ]; then
+  echo "FAIL: got exit code $exitcode, want 3"
+  exit 1
+fi
+echo PASS
