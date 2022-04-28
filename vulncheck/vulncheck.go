062fc22e0b03b8cc5bce64edc321cb850aa70a1d
Zvonimir Pavlinovic <zpavlinovic@google.com>
2022-04-28 14:43:38 -0700

    vulncheck: adds doc.go with a brief overview of vulncheck
    A more detailed overview is expected to be landed at
    go.dev/security/vulncheck
    
    Change-Id: Ie35e4a14306425789c7f9d3747ea6767ac965150
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/403137
    Reviewed-by: Julie Qiu <julie@golang.org>
    Run-TryBot: Zvonimir Pavlinovic <zpavlinovic@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- vulncheck/vulncheck.go
+++ vulncheck/vulncheck.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package vulncheck detects uses of known vulnerabilities
-// in Go binaries and source code.
 package vulncheck
 
 import (
