45f1aa055770451aa09cda5eccad075e4e541dd7
Julie Qiu <julie@golang.org>
2022-09-01 07:49:48 -0400

    vulncheck: update doc.go
    Change-Id: Ib5916d3bcf9f3673c08570ff4e362b8cee03813e
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/427474
    Reviewed-by: Julie Qiu <julieqiu@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Julie Qiu <julieqiu@google.com>
    Reviewed-by: Zvonimir Pavlinovic <zpavlinovic@google.com>
    Auto-Submit: Julie Qiu <julieqiu@google.com>

--- vulncheck/doc.go
+++ vulncheck/doc.go
@@ -4,51 +4,50 @@
 
 /*
 Package vulncheck detects uses of known vulnerabilities
-in Go programs. The two main APIs of vulncheck, Source
-and Binary, allow vulnerability detection in Go source
-code and binaries, respectively.
+in Go programs.
 
-vulncheck identifies vulnerability uses in Go programs
-at the level of call graph, package import graph, and module
-requires graph. For instance, vulncheck identifies which
-vulnerable functions and methods are transitively called
-from the program entry points. vulncheck also detects
-transitively imported packages and required modules that
-contain known vulnerable functions and methods.
+vulncheck identifies vulnerability uses in Go programs at the level of call
+graph, package import graph, and module requires graph. For instance, vulncheck
+identifies which vulnerable functions and methods are transitively called from
+the program entry points. vulncheck also detects transitively imported packages
+and required modules that contain known vulnerable functions and methods.
 
-A broader overview of vulncheck can be found at https://go.dev/security/vulncheck.
+A broader overview of vulncheck can be found at
+https://go.dev/security/vuln/vulncheck.
 
-# Inputs
+# Usage
 
-Source accepts a list of vulncheck.Package objects, which
-are a trimmed version of packages.Package objects to reduce
-memory consumption. Binary accepts a path to a Go binary file
-that must have been compiled with Go 1.18 or greater. Otherwise,
-the list of modules used by the binary is unavailable and
-vulncheck hence might miss vulnerabilities present in the binary.
+The two main APIs of vulncheck, Source and Binary, allow vulnerability
+detection in Go source code and binaries, respectively.
 
-Both Source and Binary require information about known
-vulnerabilities in the form of a vulnerability database
-https://golang.org/x/vuln/client#Client. The vulnerabilities
-are modeled using the shared https://golang.org/x/vuln/osv format.
+Source accepts a list of [Package] objects, which are a trimmed version of
+[golang.org/x/tools/go/packages.Package] objects to reduce memory consumption.
+Binary accepts a path to a Go binary file that must have been compiled with Go
+1.18 or greater. Earlier versions omit the list of modules used by the binary,
+which vulncheck needs to find vulnerabilities.
+
+Both Source and Binary require information about known vulnerabilities in the
+form of a vulnerability database, specifically a
+[golang.org/x/vuln/client.Client]. The vulnerabilities are modeled using the
+[golang.org/x/vuln/osv] format.
 
 # Results
 
-The result of vulncheck are slices of the call graph, package
-imports graph, and module requires graph leading to the use
-of an identified vulnerability. Parts of these graphs not
-related to any vulnerabilities are omitted.
+The results of vulncheck are slices of the call graph, package imports graph,
+and module requires graph leading to the use of an identified vulnerability.
+Parts of these graphs not related to any vulnerabilities are omitted.
 
 # Vulnerability Witnesses
 
-CallStacks and ImportChains APIs search the returned slices
-for user-friendly representative call stacks and import chains.
-Clients of vulncheck can use these stacks and chains as a
-witness of a vulnerability use during, for instance, security
-review.
+[CallStacks] and [ImportChains] APIs search the returned slices for
+user-friendly representative call stacks and import chains.  Clients of
+vulncheck can use these stacks and chains as a witness of a vulnerability use
+during, for instance, security review.
 
 # Limitations
 
-Please see https://go.dev/security/vulncheck#limitations.
+Please see the [documented limitations].
+
+[documented limitations]: https://go.dev/security/vulncheck#limitations.
 */
 package vulncheck
