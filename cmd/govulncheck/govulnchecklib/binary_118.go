abdd677224e440ae6df33eb6c16fbfc229c80b92
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2022-09-20 20:57:28 -0400

    cmd/govulncheck/govulnchecklib: make main functionality public
    We add this package to make integration in gopls easier.
    We do not plan to make this API stable.
    
    Change-Id: Ia0ae93520b4ef69fb3ba25ea6eb56ffb2767c673
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/432180
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Zvonimir Pavlinovic <zpavlinovic@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- cmd/govulncheck/govulnchecklib/binary_118.go
+++ cmd/govulncheck/govulnchecklib/binary_118.go
@@ -2,19 +2,18 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !go1.18
-// +build !go1.18
+//go:build go1.18
+// +build go1.18
 
-package main
+package govulnchecklib
 
 import (
 	"context"
-	"errors"
 	"io"
 
 	"golang.org/x/vuln/vulncheck"
 )
 
 func binary(ctx context.Context, exe io.ReaderAt, cfg *vulncheck.Config) (_ *vulncheck.Result, err error) {
-	return nil, errors.New("compile with Go 1.18 or higher to analyze binary files")
+	return vulncheck.Binary(ctx, exe, cfg)
 }
