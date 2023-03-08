ff8623355dfc3cd5f20abc2d3b891444f9c2a8ec
Julie Qiu <julie@golang.org>
2023-03-07 20:48:19 -0500

    internal/test: merge testenv
    Merge package testenv into test, as a step towards having only have 1
    test package.
    
    Move tests for package internal to internal_test to avoid an import
    cycle.
    
    Change-Id: I566cc9b8e99585ffbff10769dd577c64643741d9
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/474225
    Run-TryBot: Julie Qiu <julieqiu@google.com>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/goenv_test.go
+++ internal/goenv_test.go
@@ -2,19 +2,20 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package internal
+package internal_test
 
 import (
 	"testing"
 
-	"golang.org/x/vuln/internal/testenv"
+	"golang.org/x/vuln/internal"
+	"golang.org/x/vuln/internal/test"
 )
 
 func TestGoEnv(t *testing.T) {
-	testenv.NeedsGoEnv(t)
+	test.NeedsGoEnv(t)
 
 	for _, key := range []string{"GOVERSION", "GOROOT", "GOPATH", "GOMODCACHE"} {
-		if val, err := GoEnv(key); val == "" {
+		if val, err := internal.GoEnv(key); val == "" {
 			t.Errorf("want something for go env %s; got nothing", key)
 		} else if err != nil {
 			t.Errorf("unexpected error for go env %s: %v", key, err)
@@ -23,10 +24,10 @@ func TestGoEnv(t *testing.T) {
 }
 
 func TestGoEnvNonVariable(t *testing.T) {
-	testenv.NeedsGoEnv(t)
+	test.NeedsGoEnv(t)
 
 	key := "NOT_A_GO_ENV_VARIABLE"
-	if val, err := GoEnv(key); val != "" {
+	if val, err := internal.GoEnv(key); val != "" {
 		t.Errorf("expected nothing for go env %s; got %s", key, val)
 	} else if err != nil {
 		t.Errorf("unexpected error for go env %s: %v", key, err)
@@ -34,10 +35,10 @@ func TestGoEnvNonVariable(t *testing.T) {
 }
 
 func TestGoEnvErr(t *testing.T) {
-	testenv.NeedsGoEnv(t)
+	test.NeedsGoEnv(t)
 
 	key := "--not-a-flag"
-	if val, err := GoEnv(key); err == nil {
+	if val, err := internal.GoEnv(key); err == nil {
 		t.Errorf("wanted an error from go env %s; got value %q", key, val)
 	}
 }
