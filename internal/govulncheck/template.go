59f47867980adbdc403b3002405fb3337e1e5dbf
Julie Qiu <julie@golang.org>
2023-03-19 16:50:45 -0400

    internal/govulncheck: move template to files
    Move the hardcoded templates in template.go to .tmpl files.
    
    Change-Id: I0b1c4fc84a197c3760fae9e44041554039ebf1d1
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/477433
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Julie Qiu <julieqiu@google.com>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- internal/govulncheck/template.go
+++ internal/govulncheck/template.go
@@ -4,73 +4,15 @@
 
 package govulncheck
 
-// introTemplate is a text template used to communicate to the
-// user the environment used for vulnerability analysis, namely
-// the Go version, govulncheck version, vuln dbs with their last
-// modified timestamp.
-const introTemplate = `govulncheck is an experimental tool. Share feedback at https://go.dev/s/govulncheck-feedback.
+import (
+	_ "embed"
+)
 
-Using {{.GoVersion}}govulncheck{{.GovulncheckVersion}} with
-vulnerability data from {{.DB}}{{.DBLastModified}}.
-`
+//go:embed preamble.tmpl
+var introTemplate string
 
-// outputTemplate is a text template used to print out
-// govulncheck output. It consists of three sections showing
-// 1) the number of vulnerabilities detected, 2) callstacks
-// detected for each pair of module and vulnerability, and
-// 3) vulnerabilities that are only imported but not called.
-const outputTemplate = `
-{{- define "CountPhrase" -}}{{if eq (len .Affected) 1}}vulnerability{{else}}vulnerabilities{{end}}{{- end -}}
-{{- define "ModulePhrase" -}}{{if eq .AffectedModules 1}}1 module{{else}}{{.AffectedModules}} modules{{end}}{{- end -}}
-{{- define "StdlibPhrase" -}}{{if .AffectedModules}} and {{end}}the Go standard library{{- end -}}
-{{- define "VulnCount" -}}
-{{if eq (len .Affected) 0}}No vulnerabilities found.
-{{else}}Your code is affected by {{len .Affected}} {{template "CountPhrase" .}} from {{if .AffectedModules}}{{template "ModulePhrase" .}}{{end}}{{if .StdlibAffected}}{{template "StdlibPhrase" .}}{{end}}.
-{{end}}
-{{- end -}}
-
-{{- define "Affected" -}}
-{{if len .Affected}}{{range $idx, $vulnInfo := .Affected}}
-Vulnerability #{{inc $idx}}: {{$vulnInfo.ID}}
-{{wrap $vulnInfo.Details | indent 2}}
-
-  More info: https://pkg.go.dev/vuln/{{$vulnInfo.ID}}
-{{range $modInfo := $vulnInfo.Modules}}
-  {{if $modInfo.IsStd}}Standard library{{else}}Module: {{$modInfo.Module}}{{end}}
-    Found in: {{$modInfo.Found}}
-    Fixed in: {{if $modInfo.Fixed}}{{$modInfo.Fixed}}{{else}}N/A{{end}}
-    {{- if $modInfo.Platforms}}
-    Platforms: {{$modInfo.Platforms}}{{end}}
-{{if $modInfo.Stacks}}
-    Call stacks in your code:
-{{indent 6 $modInfo.Stacks}}{{end}}
-{{- end -}}
-{{- end -}}
-{{- end -}}
-{{- end -}}
-
-{{- define "Informational" -}}
-{{if len .Unaffected}}
-=== Informational ===
-
-Found {{len .Unaffected}} {{if eq (len .Unaffected) 1}}vulnerability{{else}}vulnerabilities{{end}} in packages that you import, but there are no call
-stacks leading to the use of {{if eq (len .Unaffected) 1}}this vulnerability{{else}}these vulnerabilities{{end}}. You may not need to
-take any action. See https://pkg.go.dev/golang.org/x/vuln/cmd/govulncheck
-for details.
-{{range $idx, $vulnInfo := .Unaffected}}
-Vulnerability #{{inc $idx}}: {{$vulnInfo.ID}}{{$modInfo:= index $vulnInfo.Modules 0}}
-{{wrap $vulnInfo.Details | indent 2}}
-  More info: https://pkg.go.dev/vuln/{{$vulnInfo.ID}}
-  Found in: {{$modInfo.Found}}
-  Fixed in: {{if $modInfo.Fixed}}{{$modInfo.Fixed}}{{else}}N/A{{end}}
-  {{- if $modInfo.Platforms}}
-  Platforms: {{$modInfo.Platforms}}{{end}}
-{{end}}
-{{- end -}}
-{{- end -}}
-
-{{template "VulnCount" .}}{{template "Affected" .}}{{template "Informational" . -}}
-`
+//go:embed output.tmpl
+var outputTemplate string
 
 // tmplResult is a structure containing summarized
 // govulncheck.Result, passed to outputTemplate.
