9550759f8614ff679d9415f29f0a83a5abd6d439
Julie Qiu <julie@golang.org>
2023-03-24 10:46:53 -0400

    internal/scan: add package docs for Cmd
    Package docs are added for Cmd. These are modified based on the original
    exec.Command docs.
    
    Change-Id: I504f913f74b7a38f6283ff409218b8e87339ebb9
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/479195
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Julie Qiu <julieqiu@google.com>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -12,12 +12,35 @@ import (
 	"strings"
 )
 
+// Cmd represents an external govulncheck command being prepared or run,
+// similar to exec.Cmd.
 type Cmd struct {
-	Path   string
-	Args   []string
-	Env    []string
-	Dir    string
-	Stdin  io.Reader
+	// Path is not used and exists only to model Cmd after exec.Cmd.
+	Path string
+
+	// Args holds command line arguments, including the command as Args[0].
+	// If the Args field is empty or nil, Run uses {Path}.
+	//
+	// In typical use, both Path and Args are set by calling Command.
+	Args []string
+
+	// Env is not used and exists only to model Cmd after exec.Cmd.
+	Env []string
+
+	// Dir specifies the working directory of the command.
+	//
+	// If Dir is the empty string, Run runs the command in the
+	// current directory.
+	Dir string
+
+	// Stdin specifies the standard input.
+	//
+	// If Stdin is nil, Stdin is set to os.Stdin.
+	Stdin io.Reader
+
+	// Stdout and Stderr specify the standard output and error.
+	//
+	// If either is nil, Run connects os.Stdout and os.Stderr respectively.
 	Stdout io.WriteCloser
 	Stderr io.WriteCloser
 
@@ -27,23 +50,33 @@ type Cmd struct {
 }
 
 // Command is the equivalent of exec.Command
-// It produces a struct with much of equivalent behaviors, except that instead
-// of invoking an external command when started it will instead do the
-// vulnerability scan in process.
+//
+// Command returns the Cmd struct to execute govulncheck with the given
+// arguments. It does not invoke an external command when started; the
+// vulnerability scan happens in process.
+//
+// It sets only the Path and Args in the returned structure.
+//
+// The returned Cmd's Args field is constructed from the command name (which is
+// always unused, but present to model the exec.Command API), followed
+// by the elements of arg, so arg should not include the command name itself.
+//
+// For example, Command("echo", "hello"). Args[0] is always name, not the
+// possibly resolved Path.
+//
 // It is designed to be very easy to switch to running an external command
 // instead.
 func Command(ctx context.Context, name string, arg ...string) *Cmd {
-	if ctx == nil {
-		panic("nil Context")
-	}
 	return &Cmd{
 		Path: name,
 		Args: append([]string{name}, arg...),
-
-		ctx: ctx,
+		ctx:  ctx,
 	}
 }
 
+// String returns a human-readable description of c. It is intended only for
+// debugging. In particular, it is not suitable for use as input to a shell. The
+// output of String may vary across releases.
 func (c *Cmd) String() string {
 	b := new(strings.Builder)
 	b.WriteString(c.Path)
@@ -54,6 +87,10 @@ func (c *Cmd) String() string {
 	return b.String()
 }
 
+// Run starts govulncheck and waits for it to complete.
+//
+// The returned error is nil if the command runs, has no problems copying
+// stdin, stdout, and stderr, and without an error.
 func (c *Cmd) Run() error {
 	if err := c.Start(); err != nil {
 		return err
@@ -61,6 +98,10 @@ func (c *Cmd) Run() error {
 	return c.Wait()
 }
 
+// Start starts the specified command but does not wait for it to complete.
+//
+// After a successful call to Start the Wait method must be called in order to
+// release associated system resources.
 func (c *Cmd) Start() error {
 	if c.done != nil {
 		return errors.New("vuln: already started")
@@ -90,6 +131,15 @@ func (c *Cmd) Start() error {
 	return nil
 }
 
+// Wait waits for the command to exit and waits for any copying to stdin or
+// copying from stdout or stderr to complete.
+//
+// The command must have been started by Start.
+//
+// The returned error is nil if the command runs, has no problems copying
+// stdin, stdout, and stderr, and without an error.
+//
+// Wait releases any resources associated with the Cmd.
 func (c *Cmd) Wait() error {
 	<-c.done
 	return c.err
