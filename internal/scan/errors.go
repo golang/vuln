325f27dfa07a8fc89ef47ceab1a4676a1a831852
Julie Qiu <julie@golang.org>
2023-03-31 16:41:15 -0400

    internal/scan: add mode flag
    At the moment, govulncheck tries to be smart about detecting when
    something is a binary, but when it gets this wrong, there is no way for
    the user to signal that to the tool.
    
    Add a -mode flag to explicitly distinguish between source and binary
    mode. By default, govulncheck always runs source analysis.
    
    Change-Id: I476ac245210d84a4f7e40df7f6896d7b633eb1cb
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/481137
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Julie Qiu <julieqiu@google.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- internal/scan/errors.go
+++ internal/scan/errors.go
@@ -15,6 +15,7 @@ import (
 
 var (
 	ErrMissingArgPatterns   = errors.New("missing any pattern args")
+	ErrInvalidArg           = errors.New("invalid arg")
 	ErrVulnerabilitiesFound = errors.New("vulnerabilities found")
 )
 
--- internal/scan/flags.go
+++ internal/scan/flags.go
@@ -28,17 +28,21 @@ type config struct {
 
 func (c *Cmd) parseFlags() (*config, error) {
 	cfg := &config{}
-	var tagsFlag buildutil.TagsFlag
+	var (
+		tagsFlag buildutil.TagsFlag
+		mode     string
+	)
 	flags := flag.NewFlagSet("", flag.ContinueOnError)
 	flags.BoolVar(&cfg.json, "json", false, "output JSON")
 	flags.BoolVar(&cfg.verbose, "v", false, "print a full call stack for each vulnerability")
 	flags.BoolVar(&cfg.test, "test", false, "analyze test files. Only valid for source code.")
 	flags.StringVar(&cfg.db, "db", "https://vuln.go.dev", "vulnerability database URL")
+	flags.StringVar(&mode, "mode", "", "source or binary modes are supported, default is source")
 	flags.Var(&tagsFlag, "tags", "comma-separated `list` of build tags")
 	flags.Usage = func() {
 		fmt.Fprint(flags.Output(), `usage:
 	govulncheck [flags] package...
-	govulncheck [flags] binary
+	govulncheck -mode=binary [flags] binary
 
 `)
 		flags.PrintDefaults()
@@ -53,10 +57,20 @@ func (c *Cmd) parseFlags() (*config, error) {
 		flags.Usage()
 		return nil, ErrMissingArgPatterns
 	}
-	cfg.analysis = govulncheck.AnalysisSource
-	if len(cfg.patterns) == 1 {
-		if isFile(cfg.patterns[0]) {
-			cfg.analysis = govulncheck.AnalysisBinary
+	switch mode {
+	case "binary":
+		cfg.analysis = govulncheck.AnalysisBinary
+	case "":
+		cfg.analysis = govulncheck.AnalysisSource
+	default:
+		return nil, ErrInvalidArg
+	}
+	if cfg.analysis == govulncheck.AnalysisBinary {
+		if len(cfg.patterns) != 1 {
+			return nil, ErrInvalidArg
+		}
+		if !isFile(cfg.patterns[0]) {
+			return nil, fmt.Errorf("%q is not a file", cfg.patterns[0])
 		}
 	}
 	cfg.tags = tagsFlag
