a2a74183ef471829669fb6d524047536a76ea5c4
Julie Qiu <julie@golang.org>
2023-04-04 20:31:30 -0400

    internal/scan: remove unused fields and methods from Cmd
    Remove several fields and methods from Cmd that are currently unused.
    These can be added back in the future as needed.
    
    Change-Id: I98536b1f620bd428e43a546e5106aa50a9fb3e18
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/482242
    Run-TryBot: Julie Qiu <julie@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -9,40 +9,20 @@ import (
 	"errors"
 	"io"
 	"os"
-	"strings"
 )
 
 // Cmd represents an external govulncheck command being prepared or run,
 // similar to exec.Cmd.
 type Cmd struct {
-	// Path is not used and exists only to model Cmd after exec.Cmd.
-	Path string
-
 	// Args holds command line arguments, including the command as Args[0].
 	// If the Args field is empty or nil, Run uses {Path}.
 	//
 	// In typical use, both Path and Args are set by calling Command.
 	Args []string
 
-	// Env is not used and exists only to model Cmd after exec.Cmd.
-	Env []string
-
-	// Dir specifies the working directory of the command.
-	//
-	// If Dir is the empty string, Run runs the command in the
-	// current directory.
-	Dir string
-
-	// Stdin specifies the standard input.
-	//
-	// If Stdin is nil, Stdin is set to os.Stdin.
-	Stdin io.Reader
-
-	// Stdout and Stderr specify the standard output and error.
-	//
-	// If either is nil, Run connects os.Stdout and os.Stderr respectively.
+	// Stdout specifies the standard output and error.
+	// If nil, Run connects os.Stdout.
 	Stdout io.WriteCloser
-	Stderr io.WriteCloser
 
 	ctx  context.Context
 	done chan struct{}
@@ -68,29 +48,12 @@ type Cmd struct {
 // instead.
 func Command(ctx context.Context, name string, arg ...string) *Cmd {
 	return &Cmd{
-		Path: name,
 		Args: append([]string{name}, arg...),
 		ctx:  ctx,
 	}
 }
 
-// String returns a human-readable description of c. It is intended only for
-// debugging. In particular, it is not suitable for use as input to a shell. The
-// output of String may vary across releases.
-func (c *Cmd) String() string {
-	b := new(strings.Builder)
-	b.WriteString(c.Path)
-	for _, a := range c.Args[1:] {
-		b.WriteByte(' ')
-		b.WriteString(a)
-	}
-	return b.String()
-}
-
 // Run starts govulncheck and waits for it to complete.
-//
-// The returned error is nil if the command runs, has no problems copying
-// stdin, stdout, and stderr, and without an error.
 func (c *Cmd) Run() error {
 	if err := c.Start(); err != nil {
 		return err
@@ -106,14 +69,8 @@ func (c *Cmd) Start() error {
 	if c.done != nil {
 		return errors.New("vuln: already started")
 	}
-	if c.Stdin == nil {
-		c.Stdin = os.Stdin
-	}
 	if c.Stdout == nil {
 		c.Stdout = os.Stdout
-	}
-	if c.Stderr == nil {
-		c.Stderr = os.Stderr
 	}
 	c.done = make(chan struct{})
 	go func() {
@@ -122,22 +79,14 @@ func (c *Cmd) Start() error {
 			if c.Stdout != os.Stdout {
 				c.Stdout.Close()
 			}
-			if c.Stderr != os.Stderr {
-				c.Stderr.Close()
-			}
 		}()
 		c.err = c.scan()
 	}()
 	return nil
 }
 
-// Wait waits for the command to exit and waits for any copying to stdin or
-// copying from stdout or stderr to complete.
-//
-// The command must have been started by Start.
-//
-// The returned error is nil if the command runs, has no problems copying
-// stdin, stdout, and stderr, and without an error.
+// Wait waits for the command to exit. The command must have been started by
+// Start.
 //
 // Wait releases any resources associated with the Cmd.
 func (c *Cmd) Wait() error {
