96ccd9930113d842b81513c991996a840616ec3c
Julie Qiu <julie@golang.org>
2023-04-17 13:48:34 -0400

    all: use GO_BUILDER_NAME to detect if on trybots
    Use GO_BUILDER_NAME to detect if tests are running on trybots.
    
    If not, run `./checks.bash` instead of `./checks.bash trybots`.
    
    Change-Id: I570f901b871ffde8a8292100861b202f41c0f100
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/485235
    Reviewed-by: Julie Qiu <julieqiu@google.com>
    Auto-Submit: Julie Qiu <julieqiu@google.com>
    Run-TryBot: Julie Qiu <julieqiu@google.com>
    Run-TryBot: Julie Qiu <julie@golang.org>
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- all_test.go
+++ all_test.go
@@ -27,7 +27,12 @@ func Test(t *testing.T) {
 		t.Skipf("skipping: %v", err)
 	}
 
-	cmd := exec.Command(bash, "./checks.bash", "trybots")
+	var cmd *exec.Cmd
+	if os.Getenv("GO_BUILDER_NAME") != "" {
+		cmd = exec.Command(bash, "./checks.bash", "trybots")
+	} else {
+		cmd = exec.Command(bash, "./checks.bash")
+	}
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
