golang/vuln

commit 8ddf925295301242a75c0c3bf605cb8ac4046980

cmd,internal/scan: consolidate error handling for flags
Consolidate error handling for parsing flags to validateConfig. Add
tests for the various error outputs.

For golang/go#58945
Fixes golang/go#59139

Change-Id: Ic43f30b17dc830698bce62a6177c45ee4bfb8567
Reviewed-on: https://go-review.googlesource.com/c/vuln/+/482345
Auto-Submit: Julie Qiu <julieqiu@google.com>
Run-TryBot: Julie Qiu <julie@golang.org>
Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
Reviewed-by: Julie Qiu <julieqiu@google.com>
TryBot-Result: Gopher Robot <gobot@golang.org>

internal/scan/flags.go
@@ -5,9 +5,11 @@
 package scan
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"golang.org/x/tools/go/buildutil"
 	"golang.org/x/vuln/internal/vulncheck"
@@ -30,24 +32,16 @@ const (
 	modeBinary = "binary"
 )
 
-var supportedModes = map[string]bool{
-	modeSource: true,
-	modeBinary: true,
-}
-
 func parseFlags(args []string) (*config, error) {
 	cfg := &config{}
-	var (
-		tagsFlag buildutil.TagsFlag
-		mode     string
-	)
+	var tagsFlag buildutil.TagsFlag
 	flags := flag.NewFlagSet("", flag.ContinueOnError)
 	flags.BoolVar(&cfg.json, "json", false, "output JSON")
 	flags.BoolVar(&cfg.verbose, "v", false, "print a full call stack for each vulnerability")
 	flags.BoolVar(&cfg.test, "test", false, "analyze test files (only valid for source mode)")
 	flags.StringVar(&cfg.dir, "C", "", "change to dir before running govulncheck")
 	flags.StringVar(&cfg.db, "db", "https://vuln.go.dev", "vulnerability database URL")
-	flags.StringVar(&mode, "mode", modeSource, "supports source or binary")
+	flags.StringVar(&cfg.mode, "mode", modeSource, "supports source or binary")
 	flags.Var(&tagsFlag, "tags", "comma-separated `list` of build tags")
 	flags.Usage = func() {
 		fmt.Fprint(flags.Output(), `Govulncheck is a tool for finding known vulnerabilities.
@@ -67,23 +61,45 @@ Usage:
 	cfg.patterns = flags.Args()
 	if len(cfg.patterns) == 0 {
 		flags.Usage()
-		return nil, ErrMissingArgPatterns
-	}
-	if _, ok := supportedModes[mode]; !ok {
-		return nil, ErrInvalidArg
-	}
-	cfg.mode = mode
-
-	if cfg.mode == modeBinary {
-		if len(cfg.patterns) != 1 {
-			return nil, ErrInvalidArg
-		}
-		if !isFile(cfg.patterns[0]) {
-			return nil, fmt.Errorf("%q is not a file", cfg.patterns[0])
-		}
+		return nil, ErrNoPatterns
 	}
 	cfg.tags = tagsFlag
+	if err := validateConfig(cfg); err != nil {
+		return nil, fmt.Errorf("govulncheck: %w", err)
+	}
 	return cfg, nil
+}
+
+var supportedModes = map[string]bool{
+	modeSource: true,
+	modeBinary: true,
+}
+
+func validateConfig(cfg *config) error {
+	if _, ok := supportedModes[cfg.mode]; !ok {
+		return fmt.Errorf("%q is not a valid mode", cfg.mode)
+	}
+
+	switch cfg.mode {
+	case modeSource:
+		if !fileExists(filepath.Join(cfg.dir, "go.mod")) {
+			return errNoGoMod
+		}
+	case modeBinary:
+		if cfg.test {
+			return fmt.Errorf("the -test flag is not supported in binary mode")
+		}
+		if len(cfg.tags) > 0 {
+			return fmt.Errorf("the -tags flag is not supported in binary mode")
+		}
+		if len(cfg.patterns) != 1 {
+			return fmt.Errorf("only 1 binary can be analyzed at a time")
+		}
+		if !isFile(cfg.patterns[0]) {
+			return fmt.Errorf("%q is not a file", cfg.patterns[0])
+		}
+	}
+	return nil
 }
 
 func isFile(path string) bool {
@@ -92,4 +108,18 @@ func isFile(path string) bool {
 		return false
 	}
 	return !s.IsDir()
+}
+
+// fileExists checks if file path exists. Returns true
+// if the file exists or it cannot prove that it does
+// not exist. Otherwise, returns false.
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	// Conservatively return true if os.Stat fails
+	// for some other reason.
+	return true
 }

internal/scan/source.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
-	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -27,10 +26,6 @@ func runSource(ctx context.Context, handler govulncheck.Handler, cfg *config, di
 	var pkgs []*vulncheck.Package
 	pkgs, err := loadPackages(cfg, dir)
 	if err != nil {
-		// Try to provide a meaningful and actionable error message.
-		if !fileExists(filepath.Join(dir, "go.mod")) {
-			return nil, fmt.Errorf("govulncheck: %v", errNoGoMod)
-		}
 		if isGoVersionMismatchError(err) {
 			return nil, fmt.Errorf("govulncheck: %v\n\n%v", errGoVersionMismatch, err)
 		}

cmd/govulncheck/testdata/nogomod.ct
@@ -1,10 +1,6 @@
 # Test of missing go.mod error message.
 
 $ govulncheck -C ${moddir}/nogomod . --> FAIL 1
-govulncheck is an experimental tool. Share feedback at https://go.dev/s/govulncheck-feedback.
-
-Using go1.18 and govulncheck@v0.0.0-00000000000-20000101010101 with
-vulnerability data from testdata/vulndb-v1 (last modified 01 Jan 21 00:00 UTC).
 govulncheck: no go.mod file
 
 govulncheck only works Go with modules. Try navigating to your module directory.

cmd/govulncheck/main.go
@@ -20,10 +20,12 @@ func main() {
 		switch err {
 		case flag.ErrHelp:
 			os.Exit(0)
-		case scan.ErrMissingArgPatterns:
-			os.Exit(1)
 		case scan.ErrVulnerabilitiesFound:
 			os.Exit(3)
+		case scan.ErrNoPatterns:
+			// flag.Usage is printed in the case of this error, so do not print
+			// the actual error message.
+			os.Exit(1)
 		default:
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)

cmd/govulncheck/testdata/badmode.ct
@@ -1,0 +1,4 @@
+# Test of invalid input to -mode
+
+$ govulncheck -mode=invalid ./... --> FAIL 1
+govulncheck: "invalid" is not a valid mode

cmd/govulncheck/testdata/binarybadfile.ct
@@ -1,0 +1,4 @@
+# Test of passing a non-file to -mode=binary
+
+$ govulncheck -mode=binary notafile --> FAIL 1
+govulncheck: "notafile" is not a file

cmd/govulncheck/testdata/binarymulti.ct
@@ -1,0 +1,4 @@
+# Test of trying to analyze multiple binaries
+
+$ govulncheck -mode=binary ${vuln_binary} ${vuln_binary} --> FAIL 1
+govulncheck: only 1 binary can be analyzed at a time

cmd/govulncheck/testdata/binarynotags.ct
@@ -1,0 +1,4 @@
+# Test of trying to run -mode=binary with -tags flag
+
+$ govulncheck -tags=foo -mode=binary ${vuln_binary} --> FAIL 1
+govulncheck: the -tags flag is not supported in binary mode

commit 6ffb322249035dc7f79690ca496ec2251f86f120

internal/scan: fix handling of go.mod error
Fix handling of go.mod error introduced in CL 482345.

Also add tests to prevent this issue in the future.

Change-Id: I111daa31f43c99d5ed1edbec9d5aef5a3bcf58d4
Reviewed-on: https://go-review.googlesource.com/c/vuln/+/485516
Reviewed-by: Julie Qiu <julieqiu@google.com>
Run-TryBot: Julie Qiu <julie@golang.org>
TryBot-Result: Gopher Robot <gobot@golang.org>
Reviewed-by: Zvonimir Pavlinovic <zpavlinovic@google.com>

internal/scan/flags.go
@@ -9,7 +9,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"golang.org/x/tools/go/buildutil"
 	"golang.org/x/vuln/internal/vulncheck"
@@ -80,12 +79,7 @@ func validateConfig(cfg *config) error {
 		return fmt.Errorf("%q is not a valid mode", cfg.mode)
 	}
 
-	switch cfg.mode {
-	case modeSource:
-		if !fileExists(filepath.Join(cfg.dir, "go.mod")) {
-			return errNoGoMod
-		}
-	case modeBinary:
+	if cfg.mode == modeBinary {
 		if cfg.test {
 			return fmt.Errorf("the -test flag is not supported in binary mode")
 		}

internal/scan/source.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -26,6 +27,10 @@ func runSource(ctx context.Context, handler govulncheck.Handler, cfg *config, di
 	var pkgs []*vulncheck.Package
 	pkgs, err := loadPackages(cfg, dir)
 	if err != nil {
+		// Try to provide a meaningful and actionable error message.
+		if !fileExists(filepath.Join(dir, "go.mod")) {
+			return nil, fmt.Errorf("govulncheck: %v", errNoGoMod)
+		}
 		if isGoVersionMismatchError(err) {
 			return nil, fmt.Errorf("govulncheck: %v\n\n%v", errGoVersionMismatch, err)
 		}

cmd/govulncheck/testdata/modules/vuln/subdir/subdir.go
@@ -1,0 +1,9 @@
+package subdir
+
+import (
+	"golang.org/x/text/language"
+)
+
+func Foo() {
+	language.Parse("")
+}

cmd/govulncheck/testdata/nogomod.ct
@@ -1,6 +1,10 @@
 # Test of missing go.mod error message.
 
 $ govulncheck -C ${moddir}/nogomod . --> FAIL 1
+govulncheck is an experimental tool. Share feedback at https://go.dev/s/govulncheck-feedback.
+
+Using go1.18 and govulncheck@v0.0.0-00000000000-20000101010101 with
+vulnerability data from testdata/vulndb-v1 (last modified 01 Jan 21 00:00 UTC).
 govulncheck: no go.mod file
 
 govulncheck only works Go with modules. Try navigating to your module directory.

cmd/govulncheck/testdata/source.ct
@@ -1,4 +1,4 @@
-$ govulncheck -C ${moddir}/vuln -v . --> FAIL 3
+$ govulncheck -C ${moddir}/vuln -v ./... --> FAIL 3
 govulncheck is an experimental tool. Share feedback at https://go.dev/s/govulncheck-feedback.
 
 Using go1.18 and govulncheck@v0.0.0-00000000000-20000101010101 with

cmd/govulncheck/testdata/subdir.ct
@@ -1,0 +1,28 @@
+# Test govulncheck runs on the subdirectory of a module
+
+$ govulncheck -C ${moddir}/vuln/subdir -v . --> FAIL 3
+govulncheck is an experimental tool. Share feedback at https://go.dev/s/govulncheck-feedback.
+
+Using go1.18 and govulncheck@v0.0.0-00000000000-20000101010101 with
+vulnerability data from testdata/vulndb-v1 (last modified 01 Jan 21 00:00 UTC).
+
+Scanning your code and P packages across M dependent module for known vulnerabilities...
+Your code is affected by 1 vulnerability from 1 module.
+
+Vulnerability #1: GO-2021-0113
+  Due to improper index calculation, an incorrectly formatted
+  language tag can cause Parse to panic via an out of bounds read.
+  If Parse is used to process untrusted user inputs, this may be
+  used as a vector for a denial of service attack.
+
+  More info: https://pkg.go.dev/vuln/GO-2021-0113
+
+  Module: golang.org/x/text
+    Found in: golang.org/x/text@v0.3.0
+    Fixed in: golang.org/x/text@v0.3.7
+
+    Call stacks in your code:
+      #1: for function Parse
+        golang.org/vuln/subdir.Foo
+            .../subdir.go:8:16
+        golang.org/x/text/language.Parse