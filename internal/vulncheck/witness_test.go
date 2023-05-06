47f081e3f17e6ebaeca96d30de52bdddaadc789a
Ian Cottrell <iancottrell@google.com>
2023-05-05 20:05:03 -0400

    internal/vulncheck: remove the import chains
    They are not used any more.
    
    Change-Id: Id803d0e93e36efa2145affcc7250029e7b03aacf
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/494142
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Ian Cottrell <iancottrell@google.com>
    Reviewed-by: Zvonimir Pavlinovic <zpavlinovic@google.com>
    Auto-Submit: Ian Cottrell <iancottrell@google.com>

--- internal/vulncheck/witness_test.go
+++ internal/vulncheck/witness_test.go
@@ -8,27 +8,7 @@ import (
 	"reflect"
 	"strings"
 	"testing"
-
-	"golang.org/x/tools/go/packages"
 )
-
-// chainsToString converts map Vuln:chains to Vuln.PkgPath:["pkg1->...->pkgN", ...]
-// string representation.
-func chainsToString(chains map[*Vuln][]ImportChain) map[string][]string {
-	m := make(map[string][]string)
-	for v, chs := range chains {
-		var chsStr []string
-		for _, ch := range chs {
-			var chStr []string
-			for _, imp := range ch {
-				chStr = append(chStr, imp.Package.PkgPath)
-			}
-			chsStr = append(chsStr, strings.Join(chStr, "->"))
-		}
-		m[v.ImportSink.Package.PkgPath] = chsStr
-	}
-	return m
-}
 
 // stacksToString converts map *Vuln:stacks to Vuln.Symbol:["f1->...->fN", ...]
 // string representation.
@@ -46,42 +26,6 @@ func stacksToString(stacks map[*Vuln][]CallStack) map[string][]string {
 		m[v.Symbol] = stsStr
 	}
 	return m
-}
-
-func TestImportChains(t *testing.T) {
-	// Package import structure for the test program
-	//    entry1  entry2
-	//      |       |
-	//    interm1   |
-	//      |    \  |
-	//      |   interm2
-	//      |   /     |
-	//     vuln1    vuln2
-	e1 := &PkgNode{Package: &packages.Package{ID: "1", PkgPath: "entry1"}}
-	e2 := &PkgNode{Package: &packages.Package{ID: "2", PkgPath: "entry2"}}
-	i1 := &PkgNode{Package: &packages.Package{ID: "3", PkgPath: "interm1"}, ImportedBy: []*PkgNode{e1}}
-	i2 := &PkgNode{Package: &packages.Package{ID: "4", PkgPath: "interm2"}, ImportedBy: []*PkgNode{e2, i1}}
-	v1 := &PkgNode{Package: &packages.Package{ID: "5", PkgPath: "vuln1"}, ImportedBy: []*PkgNode{i1, i2}}
-	v2 := &PkgNode{Package: &packages.Package{ID: "6", PkgPath: "vuln2"}, ImportedBy: []*PkgNode{i2}}
-	vuln1 := &Vuln{ImportSink: v1}
-	vuln2 := &Vuln{ImportSink: v2}
-	res := &Result{
-		EntryPackages: []*PkgNode{e1, e2},
-		Vulns:         []*Vuln{vuln1, vuln2},
-	}
-
-	// The chain entry1->interm1->interm2->vuln1 is not reported
-	// as there exist a shorter trace going from entry1 to vuln1
-	// via interm1.
-	want := map[string][]string{
-		"vuln1": {"entry1->interm1->vuln1", "entry2->interm2->vuln1"},
-		"vuln2": {"entry2->interm2->vuln2", "entry1->interm1->interm2->vuln2"},
-	}
-
-	chains := ImportChains(res)
-	if got := chainsToString(chains); !reflect.DeepEqual(want, got) {
-		t.Errorf("want %v; got %v", want, got)
-	}
 }
 
 func TestCallStacks(t *testing.T) {
