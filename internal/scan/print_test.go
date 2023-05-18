5a75c18bdc330546979b884e09eaec6169c8a848
Ian Cottrell <iancottrell@google.com>
2023-05-18 01:38:24 -0400

    internal/scan: add the ability to do show variant tests
    Add a test for full trace mode.
    
    Change-Id: I04ed9000c17d761bc97a079fbc3f678c7c661b88
    Reviewed-on: https://go-review.googlesource.com/c/vuln/+/495866
    Run-TryBot: Ian Cottrell <iancottrell@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Ian Cottrell <iancottrell@google.com>
    Reviewed-by: Julie Qiu <julieqiu@google.com>

--- internal/scan/print_test.go
+++ internal/scan/print_test.go
@@ -23,16 +23,27 @@ func TestPrinting(t *testing.T) {
 	}
 	for _, input := range inputs {
 		name := strings.TrimSuffix(input, ".json")
-		t.Run(name, func(t *testing.T) {
-			rawJSON, _ := fs.ReadFile(testdata, input)
-			wantText, _ := fs.ReadFile(testdata, name+".txt")
-			got := &strings.Builder{}
-			testRunHandler(t, rawJSON, scan.NewTextHandler(got))
-			if diff := cmp.Diff(string(wantText), got.String()); diff != "" {
-				t.Errorf("Readable mismatch (-want, +got):\n%s", diff)
-			}
-			got.Reset()
+		rawJSON, _ := fs.ReadFile(testdata, input)
+		textfiles, err := fs.Glob(testdata, name+"*.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, textfile := range textfiles {
+			textname := strings.TrimSuffix(textfile, ".txt")
+			t.Run(textname, func(t *testing.T) {
+				wantText, _ := fs.ReadFile(testdata, textfile)
+				got := &strings.Builder{}
+				handler := scan.NewTextHandler(got)
+				handler.Show = strings.Split(textname, "_")[1:]
+				testRunHandler(t, rawJSON, handler)
+				if diff := cmp.Diff(string(wantText), got.String()); diff != "" {
+					t.Errorf("Readable mismatch (-want, +got):\n%s", diff)
+				}
+			})
+		}
+		t.Run(name+"_json", func(t *testing.T) {
 			// this effectively tests that we can round trip the json
+			got := &strings.Builder{}
 			testRunHandler(t, rawJSON, govulncheck.NewJSONHandler(got))
 			if diff := cmp.Diff(strings.TrimSpace(string(rawJSON)), strings.TrimSpace(got.String())); diff != "" {
 				t.Errorf("JSON mismatch (-want, +got):\n%s", diff)
